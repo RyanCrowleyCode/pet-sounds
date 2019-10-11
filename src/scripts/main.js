f25f0c5928008b0457b4914850c42e4e9428c45b
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-11 13:47:53 -0500

    Represent your Pet completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
-console.log("pet sounds fun!")
+// Pet Sounds exercises!
+
+// Represent your Pet
+// Create an object that represents your pet.
+const pet = {
+    name: "Daphne",
+    species: "cat",
+    nicknames: ["Daphodil", "Sweet Girl"],
+    age: 5,
+}
